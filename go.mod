472c37bea2d6a86e4009647d5e7a7b1c98b6dd76
Vicrrs <enricofermi2019@gmail.com>
2024-04-30 01:52:09 -0400

    adding gorilla mux to have a router

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/Vicrrs/tutorial_gin
 
 go 1.22.1
+
+require github.com/gorilla/mux v1.8.1
--- routes/routes.go
+++ routes/routes.go
@@ -5,10 +5,12 @@ import (
 	"net/http"
 
 	"github.com/Vicrrs/tutorial_gin/controllers"
+	"github.com/gorilla/mux"
 )
 
 func HandleResquest() {
-	http.HandleFunc("/", controllers.Home)
-	http.HandleFunc("/api/personalidades", controllers.TodasPersonalidade)
-	log.Fatal(http.ListenAndServe(":8000", nil))
+	r := mux.NewRouter()
+	r.HandleFunc("/", controllers.Home)
+	r.HandleFunc("/api/personalidades", controllers.TodasPersonalidade)
+	log.Fatal(http.ListenAndServe(":8000", r))
 }
